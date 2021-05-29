38a527fe43c0cf8d84e0262dce8f23351cebdec4
brunomotadev <brunomotadev@gmail.com>
2021-05-29 16:30:01 -0300

    correcao de dados no fakedata

--- app/src/main/java/com/brunomotadev/santanderdevweek/data/local/FakeData.kt
+++ app/src/main/java/com/brunomotadev/santanderdevweek/data/local/FakeData.kt
@@ -5,11 +5,10 @@ import com.brunomotadev.santanderdevweek.data.Cliente
 import com.brunomotadev.santanderdevweek.data.Conta
 
 class FakeData {
-
-    fun getLocalData() : Conta {
+    fun getLocalData():Conta{
         val cliente = Cliente("Bruno")
-        val cartao = Cartao("4332432423432")
-        val conta = Conta(
+        val cartao = Cartao("6586456849564")
+        return Conta(
             "444565-4",
             "6552-4",
             "2.458,80",
@@ -17,6 +16,5 @@ class FakeData {
             cliente,
             cartao
         )
-
     }
 }
